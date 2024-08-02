bee410329525cac568fdfea7bd43f24465fd3863
Yash Gupta <109368749+yashkg31@users.noreply.github.com>
2024-08-02 14:44:11 +0530

    Revert "Delete backend/dist directory"
    This reverts commit 41f0d31f26e5d124a8c628e961f8ded66fd5eebf.

---
+++ backend/dist/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
+import { config } from 'dotenv';
+import appRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+config();
+const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan("dev")); //remove it in prod
+app.use("/api/v1", appRouter);
+export default app;
+//# sourceMappingURL=app.js.map
---
+++ backend/dist/config/open-ai-config.js
@@ -0,0 +1,9 @@
+import { Configuration } from "openai";
+export const configureOpenAI = () => {
+    const config = new Configuration({
+        apiKey: process.env.OPEN_AI_SECRET,
+        organization: process.env.OPEN_AI_ORG_ID
+    });
+    return config;
+};
+//# sourceMappingURL=open-ai-config.js.map
