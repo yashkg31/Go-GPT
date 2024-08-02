f48f4b91db19a971dd775e412a15400164f99433
Yash Gupta <109368749+yashkg31@users.noreply.github.com>
2024-08-02 18:33:06 +0530

    Update Signup.tsx

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -48,7 +48,7 @@ const Signup = () => {
   height={"90vh"}>
 
     <Box
-    width={{md : "420px", sm: "400px", xs : "360px"}}
+    width={{md : "420px", sm: "400px", xs : "320px"}}
     display={"flex"}
     justifyContent={"center"}
     alignItems={"center"}
