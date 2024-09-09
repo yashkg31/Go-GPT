f0292ccac57f38cba459c29f4995761cfeca3df4
Yash Gupta <109368749+yashkg31@users.noreply.github.com>
2024-09-09 19:13:20 +0530

    Remove dist folder from tracking

--- backend/dist/config/open-ai-config.js
+++
@@ -1,9 +0,0 @@
-import { Configuration } from "openai";
-export const configureOpenAI = () => {
-    const config = new Configuration({
-        apiKey: process.env.OPEN_AI_SECRET,
-        organization: process.env.OPEN_AI_ORG_ID
-    });
-    return config;
-};
-//# sourceMappingURL=open-ai-config.js.map
